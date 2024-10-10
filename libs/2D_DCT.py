9875fb407cde1e04791462cce929389dc4dd0a93
MicmacM <135261304+MicmacM@users.noreply.github.com>
2024-10-10 11:24:59 +0200

    same result as scipy (with a factor of 2, but not important)

--- libs/2D_DCT.py
+++ libs/2D_DCT.py
@@ -1,7 +1,9 @@
 from math import cos, pi
+import numpy as np
+from scipy.fftpack import dct
 
 def DCT_1D(x):
-    return [sum([x[n]*cos(pi/len(x)*(n + 1/2)*k) for n in range(len(x))]) for k in range(len(x))]
+    return [sum([2*x[n]*cos(pi/len(x)*(n + 1/2)*k) for n in range(len(x))]) for k in range(len(x))]
 
 def DCT_2D(x):
     X = []
@@ -13,9 +15,13 @@ def DCT_2D(x):
                 c1 = cos(pi/N*(n1 + 1/2)*k1)
                 for n2 in range(N):
                     c2 = cos(pi/N*(n2 + 1/2)*k2)
-                    Xk1k2 += x[n1,n2]*c1*c2
-n = 8
+                    Xk1k2 += x[n1,n2]*c1*c2*2
+n = 16
 l = [4*i for i in range(n)]
 
 print(DCT_1D(l))
+
+dct_result = dct(l, type=2)
+
+print(dct_result)
 
