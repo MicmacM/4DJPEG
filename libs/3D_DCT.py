3f3e3b1acd1f122e4af44b25bd8104bd7c57a49a
MicmacM <135261304+MicmacM@users.noreply.github.com>
2024-10-13 19:22:49 +0200

    Fixed some minor bugs. The compression works really well when there is no drastic temporal change, thus a way of improvment could be to select the temporal_chunk size to be dependant of the variation of the content. Another way of improvment would be to define the quantization matrix in 3D better. Also the dc coefficient compression is useless as of now since they are still present in the ac

--- libs/3D_DCT.py
+++ libs/3D_DCT.py
@@ -39,7 +39,7 @@ def quantization_matrix_3D(quantization_matrix_2D):
     for i in range(8):
         for j in range(8):
             for t in range(8):
-                Q[i,j,t] = quantization_matrix_2D[i,j] + sqrt(i**2 + j**2 + t**2)*t
+                Q[i,j,t] = int(quantization_matrix_2D[i,j] + sqrt(i**2 + j**2 + t**2)*t)
 
     return Q
 
@@ -125,8 +125,27 @@ def zigzag_encoding(matrix):
                 encoded_b[zz[i,j,k]] = matrix[i,j,2,k]
     return encoded_r, encoded_g, encoded_b
 
+def zigzag_decoding(encoded_r, encoded_g, encoded_b):
+    zz = manhattan_zigzag_matrix()
+    matrix = np.zeros((8,8,3,8))
+    for i in range(8):
+        for j in range(8):
+            for k in range(8):
+                matrix[i,j,0,k] = encoded_r[zz[i,j,k]]
+                matrix[i,j,1,k] = encoded_g[zz[i,j,k]]
+                matrix[i,j,2,k] = encoded_b[zz[i,j,k]]
+    return matrix
+
 def delta_encoding(l):
     return [l[i] if i == 0 else l[i] - l[i-1] for i in range(len(l))]
+
+def delta_decoding(l):
+    base = l[0]
+    res = [base]
+    for i in range(1,len(l)):
+        res.append(res[-1] + l[i])
+    return res
+
 
 def run_length_encoding(l):
     res = []
@@ -140,6 +159,12 @@ def run_length_encoding(l):
     res.append((l[-1], nb_elem))
     return res
 
+def run_length_decoding(l):
+    res = []
+    for (e,n) in l:
+        res += [e]*n
+    return res
+
 
 def matrix_size_in_bits(matrix):
     # Get size in bytes
@@ -150,7 +175,7 @@ def matrix_size_in_bits(matrix):
     
     return size_in_bits
 
-def number_of_bits(lst):
+def number_of_bits_ac(lst):
     total_bits = 0
     for compressed_8x8_array in lst:
         for chanel_arrays in compressed_8x8_array:
@@ -159,41 +184,69 @@ def number_of_bits(lst):
                     total_bits += int(num).bit_length()  # Sum the bit length of each integer
     return total_bits
 
+def number_of_bits_dc(lst):
+    total_bits = 0
+    for dc in lst:
+        for comp in dc:
+            total_bits += int(comp).bit_length()  # Sum the bit length of each integer
+    return total_bits
+
 
 if __name__ == "__main__":
     video_path = "../cresson.mp4"
     base_x = 800
     base_y = 350
-    increment = 12
+    #Number of spatial chunk of size 8
+    spatial_i = 12
+    t0 = 2
+    #Number of temporal chunk of size 8
+    temporal_i = 10
     test_array = time_array.video_to_frames_array(video_path)
-    compressed_data = np.zeros((increment*8, increment*8, 3, 8)).astype(int)
-    reconstitued = np.zeros((increment*8, increment*8, 3, 8)).astype(int)
-    echantillon = test_array[base_x:base_x + increment*8, base_y:base_y + increment*8, :, 2:10]
+    compressed_data = np.zeros((spatial_i*8, spatial_i*8, 3, temporal_i*8)).astype(int)
+    reconstitued = np.zeros((spatial_i*8, spatial_i*8, 3, temporal_i*8)).astype(int)
+    echantillon = test_array[base_x:base_x + spatial_i*8, base_y:base_y + spatial_i*8, :, t0:t0+(temporal_i+1)*8]
     dc_values = []
     ac_values = []
-    for i in range(increment):
-        for j in range(increment):
-            sample = test_array[base_x + i*8:base_x + 8*(i+1), base_y + 8*j:base_y + 8*(j+1), :, 2:10]
-            compressed = quantize(sample, Q)
-            ac = [compressed[x,y,:,z] for x in range(8) for y in range(8) for z in range(8)]
-            dc = ac.pop(0)
-            dc_values.append(dc)
+    for i in range(spatial_i):
+        for j in range(spatial_i):
+            for t in range(temporal_i):
+                sample = test_array[base_x + i*8:base_x + 8*(i+1), base_y + 8*j:base_y + 8*(j+1), :, t0+t*8:t0+(t+1)*8]
+                
+                #! 3D DCT COMPRESSION AND QUANTIZATION
+                compressed = quantize(sample, Q)
 
-            #? We take the dc value too, remove it in a later version perhaps ?
-            zz_compressed_r, zz_compressed_g, zz_compressed_b = zigzag_encoding(compressed)
-            compressed_ac_r = run_length_encoding(zz_compressed_r)
-            compressed_ac_g = run_length_encoding(zz_compressed_r)
-            compressed_ac_b = run_length_encoding(zz_compressed_r)
-            ac_values.append((compressed_ac_r, compressed_ac_b, compressed_ac_g))
+                #! RUN LENGTH, DELTA AND ZIGZAG ENCODING
+                ac = [compressed[x,y,:,z] for x in range(8) for y in range(8) for z in range(8)]
+                dc = ac.pop(0)
 
-            decompressed = unquantize(compressed, Q)
-            reconstitued[8*i:8*(i+1), 8*j:8*(j+1), :, :] = decompressed
+                dc_values.append(dc)
+
+                #? We take the dc value too, remove it in a later version perhaps ?
+                zz_compressed_r, zz_compressed_g, zz_compressed_b = zigzag_encoding(compressed)
+                compressed_ac_r = run_length_encoding(zz_compressed_r)
+                compressed_ac_g = run_length_encoding(zz_compressed_g)
+                compressed_ac_b = run_length_encoding(zz_compressed_b)
+                ac_values.append((compressed_ac_r, compressed_ac_b, compressed_ac_g))
+
+                #! RUN LENGTH, DELTA AND ZIGZAG DECODING
+                decompressed_ac_r = run_length_decoding(compressed_ac_r)
+                decompressed_ac_g = run_length_decoding(compressed_ac_g)
+                decompressed_ac_b = run_length_decoding(compressed_ac_b)
+
+                decompressed_zz = zigzag_decoding(decompressed_ac_r, decompressed_ac_g, decompressed_ac_b)
+
+                #! 3D IDCT AND DEQUANTIZATION
+                decompressed = unquantize(decompressed_zz, Q)
+                reconstitued[8*i:8*(i+1), 8*j:8*(j+1), :, 8*t:8*(t+1)] = decompressed
     
     dc_values = delta_encoding(dc_values)
-    print(dc_values)
-    print(number_of_bits(ac_values))
+    #! EOF
+
+    #! DEBUG ONLY
+    print(number_of_bits_ac(ac_values))
+    print(number_of_bits_dc(dc_values))
     #print(ac_values)
     print(matrix_size_in_bits(echantillon))
-    #visualize_frames_as_video(echantillon, interval=500)
-    #visualize_frames_as_video(reconstitued, interval=500)
-    print("Compression level : ", (1- (number_of_bits(ac_values)+number_of_bits(dc_values))/matrix_size_in_bits(echantillon))*100, "%")
+    visualize_frames_as_video(echantillon, interval=500)
+    visualize_frames_as_video(reconstitued, interval=500)
+    print("Compression level : ", (1- (number_of_bits_ac(ac_values)+number_of_bits_dc(dc_values))/matrix_size_in_bits(echantillon))*100, "%")
